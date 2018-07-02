ef3818e0ed817a33cbcf53c747623400707ca669
Wachirawit Chankaew <wachirawit.chankaew@gmail.com>
2018-07-02 21:44:59 +0700

    Add Resource.

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Web.Controllers
 {
@@ -6,7 +9,28 @@ namespace Web.Controllers
     {
         public IActionResult Index()
         {
+            var requestCultureFeature = HttpContext.Features.Get<IRequestCultureFeature>();
+            var culture = requestCultureFeature.RequestCulture.UICulture;
+
             return View();
+        }
+
+        public IActionResult SetThai()
+        {
+            var cookieValue = CookieRequestCultureProvider.MakeCookieValue(new RequestCulture("th-TH"));
+            var option = new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) };
+
+            Response.Cookies.Append("Web.Language", cookieValue, option);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult SetEnglish()
+        {
+            var cookieValue = CookieRequestCultureProvider.MakeCookieValue(new RequestCulture("en-GB"));
+            var option = new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) };
+
+            Response.Cookies.Append("Web.Language", cookieValue, option);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Web
 {
@@ -17,6 +21,8 @@ namespace Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -32,6 +38,28 @@ namespace Web
             }
 
             app.UseStaticFiles();
+
+            var cultures = new List<CultureInfo>
+            {
+                new CultureInfo("en-GB"),
+                new CultureInfo("th-TH")
+            };
+
+            var requestCulture = new RequestCulture("en-GB", "en-GB");
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = requestCulture,
+                SupportedCultures = cultures,
+                SupportedUICultures = cultures,
+                RequestCultureProviders = new List<IRequestCultureProvider>
+                {
+                    new CookieRequestCultureProvider
+                    {
+                        CookieName = "Web.Language"
+                    }
+                }
+            });
 
             app.UseMvc(routes =>
             {
